7d6db8fc0bad3488c6288cc0a576263463dbe33d
ghoshbinayak <ghoshbinayak@gmail.com>
2013-05-25 02:54:41 +0530

    added jquery and mobile detection

--- index.php
+++ index.php
@@ -6,17 +6,17 @@ Samudranil Roy --Photography
 </title>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 <script type="text/javascript">
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
- $("body").html("its a mobile device! :D");
-}
-else{
-    $("body").html("boo such a old form factot! :(");
-}
+$(function(){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+     $("body").html("its a mobile device! :D");
+    }
+    else{
+        $("body").html("boo such a old form factot! :(");
+    }
+})
 </script>
 <head>
 <body>
-hi!
-git test
 </body>
 </html>
 
