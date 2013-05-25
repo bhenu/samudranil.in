c9c1f0af813a484a3c93b3e81140a882b9a2400d
ghoshbinayak <ghoshbinayak@gmail.com>
2013-05-25 23:12:51 +0530

    started mobiledevelopment

--- smartphone.php
+++ smartphone.php
@@ -1,1 +1,25 @@
-<?php echo "smartphone"; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<title> Samudranil Roy -- Photography</title>
+<meta charset="UTF-8" />
+<link rel="shortcut icon" href="images/favicon.ico" />
+<link rel="stylesheet" href="css/main.css" />
+<link rel="stylesheet" href="css/smartphone.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+</head>
+<body>
+  <div class="header">
+    <div id="logo"></div>
+    <div id="menu"></div>
+  </div>
+  <div class="content-area">
+    <div class="photo-slider"></div>
+    <div class="albums"></div>
+    <div class="blog-posts"></div>
+  </div>
+  <div class="footer">
+    <a>site credits</a>
+  </div>
+</body>
+</html>
