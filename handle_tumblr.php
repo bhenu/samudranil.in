01d10453e937b35ff93706fb1edc5f22a58012f7
ghoshbinayak <ghoshbinayak@gmail.com>
2013-06-06 17:01:00 +0530

    some progress on tumblr handling

--- handle_tumblr.php
+++ handle_tumblr.php
@@ -1,8 +1,20 @@
 <?php
 
 class Tumblr(){
-	private $userid = "ghoshbinayak";
+	private $userid = "ghoshbinayak.tumblr.com";
+	private $apikey = "upraHHL2RL1JwKyg9LXX1TGyeJ8d0wZcFOus3xBf7x47pX1xyw";
+	public function getPosts($offset, $maxresults){
+		$feedURL = "api.tumblr.com/v2/blog/"
+					.$this->userid
+					."/posts?api_key="
+					.$this->apikey
+					."&limit="
+					.$maxresult
+					."&offset="
+					.$offset;
+		$response = json_decode(file_get_contents($feedURL));
 	
+	}
 
 
 }
