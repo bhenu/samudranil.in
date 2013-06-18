9486a08f19dd8d6e117991e07a3341f08da9b9f1
ghoshbinayak <ghoshbinayak@gmail.com>
2013-06-18 10:32:19 +0530

    ajax test successful

--- test.php
+++ test.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-		<script>
-			$(function(){
-				var SROY = {
-					'url' :'ajax.php',
-					'sendRequest' : function(){
-						$.getJSON(this.url, this.sendData, function(data){
-							$("#container").html("<pre>"+ data.url + "</pre>");
-							})
-						},
-					'sendData' : {
-						'type' : 'photo',
-						'photoid' : '5890319400538265762'
-						}					
-					};
-				
-				SROY.sendRequest();
-				
-				});
-		</script>
-	</head>
-	<body>
-		<p id='container'>content goes here..</p>
-	</body>
+    <head>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>
+            $(function(){
+                var SROY = {
+                    'url' :'ajax.php',
+                    'sendRequest' : function(){
+                        $.getJSON(this.url, this.sendData, function(data){
+                            $("#container").html("<pre>"+ data.url + "</pre>");
+                            })
+                        },
+                    'sendData' : {
+                        'type' : 'albums',
+                        'thumbsize' : '256c'
+                        }
+                    };
+
+                SROY.sendRequest();
+
+                });
+        </script>
+    </head>
+    <body>
+        <p id='container'>content goes here..</p>
+    </body>
 
 
 </html>
