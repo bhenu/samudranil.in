7f1e7b9ff859069a144c82f989adb3a6b215b3cf
ghoshbinayak <ghoshbinayak@gmail.com>
2013-06-18 11:35:45 +0530

    testing

--- test.php
+++ test.php
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-        <script>
-            $(function(){
-                var SROY = {
-                    'url' :'ajax.php',
-                    'sendRequest' : function(){
-                        $.getJSON(this.url, this.sendData, function(data){
-                            $("#container").html("<pre>"+ data.url + "</pre>");
-                            })
-                        },
-                    'sendData' : {
-                        'type' : 'albums',
-                        'thumbsize' : '256c'
-                        }
-                    };
-
-                SROY.sendRequest();
-
-                });
-        </script>
-    </head>
-    <body>
-        <p id='container'>content goes here..</p>
-    </body>
-
-
-</html>
+<?php
+require_once "handle_picasa.php";
+print_r($Picasa.getAlbumPhotos('5632182463740846529', '640', '1', '7');
+?>
