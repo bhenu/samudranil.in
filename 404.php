89ee0e1ee56e58bae706e5bb27f610e30be3c455
ghoshbinayak <ghoshbinayak@gmail.com>
2013-06-20 11:56:09 +0530

    blog page added

--- 404.php
+++ 404.php
@@ -6,19 +6,20 @@
 <meta name='description' content='Samudranil Roy is a  hobby photographer. This website contains some of his works and also his blogs.'>
 <meta name='keywords' content='Samudranil Roy, Samudranil, Photography, Nature Photography, photos'>
 <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
-<link rel="shortcut icon" href="../images/favicon.ico" />
-<link rel="stylesheet" href="../css/main.css" />
-<link rel="stylesheet" href="../css/smartphone.css" />
+<link rel="shortcut icon" href="http://localhost/images/favicon.ico" />
+<link rel="stylesheet" href="http://localhost/css/main.css" />
+<link rel="stylesheet" href="http://localhost/css/smartphone.css" />
 </head>
 <body>
   <div class="header">
-    <a href="../index.php"><div id="logo"><img src="../images/logo_m.png" /></div></a>
+    <a href="http://localhost/index.php"><div id="logo"><img src="http://localhost/images/logo_m.png" /></div></a>
     <div id="menu"></div>
   </div>
   <div class="menu-list">
-        <a href="../about/">about</a>
-        <a href="../blog/">blog</a>
-        <a href="../albums/">albums</a>
+        <a href="http://localhost/">home</a>
+        <a href="http://localhost/albums/">albums</a>
+        <a href="http://localhost/blog/">blog</a>
+        <a href="http://localhost/about/">about</a>
   </div>
   <p style="color: white; font-size: 60px">stop fiddling with the url!</p>
 </body>
