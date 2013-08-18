540e96268394de74e9af2535b67ebfb7b0372a5a
ghoshbinayak <ghoshbinayak@gmail.com>
2013-08-19 00:32:15 +0530

    added keyboard support

--- computer.php
+++ computer.php
@@ -77,6 +77,25 @@ require_once "handle_tumblr.php";
                                                 });
             };
 
+        // keyboard handling for scroll bar ----------------------------
+        $(document).keydown(function(e){
+                    if($(this).data("keyboard-input")==="enabled"){
+                        var activeElem=$(".keyboard-input"),
+                            activeElemPos=Math.abs($(".keyboard-input .mCSB_container").position().top),
+                            pixelsToScroll=60;
+                        if(e.which===38){ //scroll up
+                            e.preventDefault();
+                            if(pixelsToScroll>activeElemPos){
+                                activeElem.mCustomScrollbar("scrollTo","top");
+                            }else{
+                                activeElem.mCustomScrollbar("scrollTo",(activeElemPos-pixelsToScroll),{scrollInertia:400,scrollEasing:"easeOutCirc"});
+                            }
+                        }else if(e.which===40){ //scroll down
+                            e.preventDefault();
+                            activeElem.mCustomScrollbar("scrollTo",(activeElemPos+pixelsToScroll),{scrollInertia:400,scrollEasing:"easeOutCirc"});
+                        }
+                    }
+                });
 
         // ajax loading function ---------------------------------------
         var ajax_load = function(url){
