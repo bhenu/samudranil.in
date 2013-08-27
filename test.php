8090e705ee26b9fbb70c03e3d682a774cc6383cf
ghoshbinayak <ghoshbinayak@gmail.com>
2013-08-28 01:43:31 +0530

    testing

--- test.php
+++ test.php
@@ -1,41 +1,8 @@
-<!--<!DOCTYPE html>
-<html>
-	<head>
-		<title>test ajax</title>
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-		<script>
-			$(function(){
-				var Samudranil = {
-					"ajax_url" : "../ajax.php",
-					"ajax_data": {},							//data to send in post/get request
-					"ajax_success" : function(){
-						$("#content").html("<pre>"+arguments[0]+"</pre>");
-						},
-					"get_content" : function(){				//requset handler
-						$.getJSON(this.ajax_url, this.ajax_data, this.ajax_success());
-						}
-					};
-					
-				Samudranil.ajax_data = {type: 'photo', 
-										photoid: '5890319400538265762'
-									}
-				Samudranil.get_content();
-			});
-		</script>
-	</head>
-	<body>
-		<div id="content">
-			<p>content goes here</p>
-		</div>
-	</body>
-</html>
-
--->
 <?php
 require_once "handle_tumblr.php";
 require_once "handle_picasa.php";
 echo "<pre>";
-$photos = $Tumblr->getSinglePost('53442472180');
+$photos = $Picasa->getAlbums("200");
 print_r($photos);
 echo "</pre>";
 ?>
